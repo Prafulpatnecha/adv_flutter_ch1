8b3e2fdc5ce385252d8f71b29c9c904962217ad2
Praful Patnecha <prafulpatnecha@gmail.com>
2024-07-05 19:20:24 +0530

    05-07-24

--- lib/controller/controlling.dart
+++ lib/controller/controlling.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    Timer.periodic(Duration(milliseconds: 1), (timer) {
+    Timer.periodic(Duration(microseconds: 1), (timer) {
       setState(() {
 
       });
