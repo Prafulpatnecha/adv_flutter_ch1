920d75417ea24a1e01d11854664beeefff11759e
Praful Patnecha <prafulpatnecha@gmail.com>
2024-07-17 10:05:28 +0530

    17-07-24

--- lib/contact_us_page/view/home/contact_page.dart
+++ lib/contact_us_page/view/home/contact_page.dart
@@ -1,10 +1,13 @@
+import 'package:adv_flutter_ch1/quotesdata/provider/provider_qoutes_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ContactPage extends StatelessWidget {
   const ContactPage({super.key});
-
   @override
   Widget build(BuildContext context) {
+  ProviderQuotesPage providerQuoteTrue=Provider.of<ProviderQuotesPage>(context,listen: true);
+  ProviderQuotesPage providerQuoteFalse=Provider.of<ProviderQuotesPage>(context,listen: false);
     return Scaffold(
       body: Column(
         children: [
@@ -21,37 +24,47 @@ class ContactPage extends StatelessWidget {
           const SizedBox(height: 40,),
           const Text('If you have any queries, get in touch with \nus. We will be happy to help you!',textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
           const SizedBox(height: 40,),
-          Container(
-            height: 70,
-            width: 320,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Colors.blue),
-            ),
-            child: const Row(
-              children: [
-                SizedBox(width: 25,),
-                Icon(Icons.ad_units_outlined,color: Colors.blue,size: 30,),
-                SizedBox(width: 25,),
-                Text('+91 63551 99097',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 20),)
-              ],
+          GestureDetector(
+            onTap: () {
+              providerQuoteFalse.phoneNumber();
+            },
+            child: Container(
+              height: 70,
+              width: 320,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.blue),
+              ),
+              child: const Row(
+                children: [
+                  SizedBox(width: 25,),
+                  Icon(Icons.ad_units_outlined,color: Colors.blue,size: 30,),
+                  SizedBox(width: 25,),
+                  Text('+91 63551 99097',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 20),)
+                ],
+              ),
             ),
           ),
           const SizedBox(height: 20,),
-          Container(
-            height: 70,
-            width: 320,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Colors.blue),
-            ),
-            child: const Row(
-              children: [
-                SizedBox(width: 25,),
-                Icon(Icons.local_post_office_outlined,color: Colors.blue,size: 30,),
-                SizedBox(width: 25,),
-                Text('prafulpatnecha@gmail.com',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 17),)
-              ],
+          GestureDetector(
+            onTap: () {
+              providerQuoteFalse.emailMethod();
+            },
+            child: Container(
+              height: 70,
+              width: 320,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.blue),
+              ),
+              child: const Row(
+                children: [
+                  SizedBox(width: 25,),
+                  Icon(Icons.local_post_office_outlined,color: Colors.blue,size: 30,),
+                  SizedBox(width: 25,),
+                  Text('prafulpatnecha@gmail.com',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 17),)
+                ],
+              ),
             ),
           ),
           const SizedBox(height: 30,),
@@ -63,11 +76,66 @@ class ContactPage extends StatelessWidget {
             ),
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text('Social Media',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 17),),
+                const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text('Social Media',style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 20),),
                 ),
-                Divider(color: Colors.blue,),
+                const Divider(color: Colors.blue,),
+                GestureDetector(
+                  onTap: () {
+                    providerQuoteFalse.behanceWeb();
+                  },
+                  child: Row(children: [
+                    const SizedBox(width: 25,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Image.asset('assets/img/imgs(2).png'),
+                    ),
+                    const SizedBox(width: 25,),
+                    const Text('PrafulPatnecha',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 18),)
+                  ],),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Divider(color: Colors.blue,),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    providerQuoteFalse.instaWeb();
+                  },
+                  child: Row(children: [
+                    const SizedBox(width: 28,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Image.asset('assets/img/imgs(3).png',width: 35,),
+                    ),
+                    const SizedBox(width: 27,),
+                    const Text('PrafulPatnecha',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 18),)
+                  ],),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Divider(color: Colors.blue,),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    providerQuoteFalse.sportWeb();
+                  },
+                  child: Row(children: [
+                    const SizedBox(width: 35,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Image.asset('assets/img/imgs(1).png',width: 25,),
+                    ),
+                    const SizedBox(width: 30,),
+                    const Text('Leo Natsume',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 18),)
+                  ],),
+                ),
+                SizedBox(height: 10,)
+                // const Padding(
+                //   padding: EdgeInsets.symmetric(horizontal: 25),
+                //   child: Divider(color: Colors.blue,),
+                // ),
               ],
             )
           ),
--- lib/quotesdata/provider/provider_qoutes_page.dart
+++ lib/quotesdata/provider/provider_qoutes_page.dart
@@ -1,82 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../main.dart';
 import '../modal/modal_qoutes_page.dart';
 
-class ProviderQuotesPage extends ChangeNotifier
-{
-  bool introBool=false;
-  int index=0;
+class ProviderQuotesPage extends ChangeNotifier {
+  bool introBool = false;
+  int index = 0;
   late SharedPreferences sharedPreferences;
-  bool isDark=false;
-  List quotesAddingList=[];
-  void addUserList(QuotesModal quotesModal)
-  {
-    quotesAddingList.insert(0, {'quote':quotesModal.quote,'author':quotesModal.author});
+  bool isDark = false;
+  List quotesAddingList = [];
+
+  void addUserList(QuotesModal quotesModal) {
+    quotesAddingList
+        .insert(0, {'quote': quotesModal.quote, 'author': quotesModal.author});
     notifyListeners();
   }
-  void quotesAddingJsonFile(List jsonList)
-  {
+
+  void quotesAddingJsonFile(List jsonList) {
     quotesAddingList.addAll(jsonList);
     // notifyListeners();
     // print(quotesAddingList);
   }
-  void provideRemove(int index)
-  {
+
+  void provideRemove(int index) {
     quotesAddingList.removeAt(index);
     notifyListeners();
   }
-  void darkAndLightMode(bool value)
-  {
-    isDark=value;
+
+  void darkAndLightMode(bool value) {
+    isDark = value;
     setTheme(isDark);
     notifyListeners();
   }
-  Future<void> setTheme(bool value)
-  async {
-    sharedPreferences=await SharedPreferences.getInstance();
+
+  Future<void> setTheme(bool value) async {
+    sharedPreferences = await SharedPreferences.getInstance();
     sharedPreferences.setBool('theme', value);
     notifyListeners();
   }
-  Future<void> getTheme()
-  async {
-    sharedPreferences=await SharedPreferences.getInstance();
-    isDark=sharedPreferences.getBool('theme') ?? false;
+
+  Future<void> getTheme() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    isDark = sharedPreferences.getBool('theme') ?? false;
     notifyListeners();
   }
-  ProviderQuotesPage()
-  {
-    isDark=theme;
-    introBool=intro;
+
+  ProviderQuotesPage() {
+    isDark = theme;
+    introBool = intro;
     getIntro();
     getTheme();
     notifyListeners();
   }
-  void indexShare()
-  {
+
+  void indexShare() {
     index++;
     notifyListeners();
   }
-  void introBoolCheck()
-  {
-    introBool=true;
+
+  void introBoolCheck() {
+    introBool = true;
     introPageSkip(introBool);
     notifyListeners();
   }
-  void introBoolCheckOn()
-  {
-    introBool=false;
+
+  void introBoolCheckOn() {
+    introBool = false;
     introPageSkip(introBool);
     notifyListeners();
   }
-  Future<void> introPageSkip(bool value)
-  async {
-    sharedPreferences=await SharedPreferences.getInstance();
+
+  Future<void> introPageSkip(bool value) async {
+    sharedPreferences = await SharedPreferences.getInstance();
     sharedPreferences.setBool('intro', value);
   }
-  Future<void> getIntro()
-  async {
-    sharedPreferences=await SharedPreferences.getInstance();
-    introBool=sharedPreferences.getBool('intro')?? false;
+
+  Future<void> getIntro() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    introBool = sharedPreferences.getBool('intro') ?? false;
+  }
+
+  void phoneNumber() {
+    Uri url = Uri.parse('tel: +91 6355199097');
+    launchUrl(url);
+  }
+
+  void emailMethod() {
+    Uri url = Uri(
+      scheme: 'mailto',
+      path: 'prafulpatnecha@gmail.com',
+    );
+    launchUrl(url);
+  }
+  void behanceWeb()
+  {
+    Uri url=Uri.parse('https://www.behance.net/prafulpatnecha');
+    launchUrl(url);
+  }
+  void instaWeb()
+  {
+    Uri url=Uri.parse('https://www.instagram.com/prafulpatnecha');
+    launchUrl(url,mode: LaunchMode.externalNonBrowserApplication);
+  }
+  void sportWeb()
+  {
+    Uri url=Uri.parse('https://dribbble.com/shots/24532478-Spotify-All-Ears-on-you-2');
+    launchUrl(url,mode: LaunchMode.inAppWebView);
   }
 }
