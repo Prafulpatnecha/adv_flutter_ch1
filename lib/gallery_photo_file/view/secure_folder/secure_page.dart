a993d6a3ef6982fe84adb684cb9e6ad9811871d5
Praful Patnecha <prafulpatnecha@gmail.com>
2024-07-18 12:38:38 +0530

    18-07-24

---
+++ lib/gallery_photo_file/view/secure_folder/secure_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class SecurePage extends StatelessWidget {
+  const SecurePage ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Secure Page Is Open')
+      ),
+    );
+  }
+}
